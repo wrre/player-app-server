cbf183115de2a4358d743e1b010ce940062915d6
Peggy Liu <fnoihtlanl@gmail.com>
2017-09-14 12:02:09 +0800

    Add get data by limit API

--- router/api.js
+++ router/api.js
@@ -2,15 +2,22 @@ var express = require('express');
 var router = express.Router();
 var PlayerDB = require('../lib/playerdb');
 
+var player = new PlayerDB();
+
 router.get('/data', function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  var player = new PlayerDB();
 
 	player.getData(function(err, results) {
     res.send(results);
   });
+});
 
+router.get('/data/:skipNum/:limitNum', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
 
+	player.getDataByLimit(req.params, function(err, results) {
+    res.send(results);
+  });
 });
 
 module.exports = router;
